dace9f412dca0292fee60dbd70c0da8c05edc52f
Brandon Wilson <brandon@coil.com>
2020-05-08 18:14:47 -0500

    chore(web-monetization-demo): remove mini accounts plugin
    This currently causes build:ts to fail with:
    
    src/server/StreamServer.ts(21,46): error TS2741: Property 'isConnected' is missing in type 'import("/home/brandon/web-monetization-projects/node_modules/ilp-plugin/dist/index").Plugin' but required in type 'import("/home/brandon/ilp-protocol-stream/dist/src/util/plugin-interface").Plugin'.

--- packages/web-monetization-demo/src/server/StreamServer.ts
+++ packages/web-monetization-demo/src/server/StreamServer.ts
@@ -1,12 +1,11 @@
 import { inject, injectable } from 'inversify'
-import AbstractBtpPlugin from 'ilp-plugin-btp'
+import makePlugin from 'ilp-plugin'
 import {
   Connection,
   createServer,
   DataAndMoneyStream,
   Server
 } from 'ilp-protocol-stream'
-import MiniAccountsPlugin from 'ilp-plugin-mini-accounts'
 import { SPSPResponse } from '@web-monetization/types'
 
 import * as tokens from './tokens'
@@ -14,25 +13,12 @@ import { dbg } from './logging'
 
 @injectable()
 export class StreamServer {
-  server!: AbstractBtpPlugin
   streamServer!: Server
 
   constructor(@inject(tokens.BtpPort) private port: number) {}
 
   async start() {
-    this.server = new MiniAccountsPlugin({
-      debugHostIldcpInfo: {
-        clientAddress: 'local.test',
-        assetCode: 'USD',
-        assetScale: 9
-      },
-      allowedOrigins: ['.*'],
-      generateAccount: true,
-      wsOpts: {
-        port: this.port
-      }
-    })
-    this.streamServer = await createServer({ plugin: this.server })
+    this.streamServer = await createServer({ plugin: makePlugin() })
     this.streamServer.on('connection', (connection: Connection) => {
       connection.on('stream', async (stream: DataAndMoneyStream) => {
         let total = 20e3
